bb0e142b69a268fb89eb42aba0c60197c5900e57
Vishal Chauhan <vshlchauhab@gmail.com>
2021-09-29 15:33:33 +0530

    anime-quote-API-used

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const responsesBody = document.querySelector("#responses");
 
-fetch("https://api.adviceslip.com/advice", {
+fetch("https://animechan.vercel.app/api/random", {
   method: "GET",
 })
   .then((res) => res.json())
@@ -9,16 +9,16 @@ fetch("https://api.adviceslip.com/advice", {
         <div class="col-lg-12 mb-4">
             <div class="card">
               <div class="card-body">
-                <h4 class="card-title">Random Advice API</h4>
-                <h5 class="card-text mt-4">${data.slip.advice}</h5>
+                <h4 class="card-title">Anime Quote API</h4>
+                <h5 class="card-text mt-4">${data.quote}</h5>
                 <br />
-                <a href="https://github.com/ashutoshkrris" class="username">
+                <a href="https://github.com/vish-han" class="username">
                   <img
-                    src="https://avatars.githubusercontent.com/u/47353498?v=4"
+                    src="https://avatars.githubusercontent.com/u/44545745?v=4"
                     alt="DP"
                     class="rounded-circle img-fluid mr-2"
                     width="40"
-                    height="40"/>ashutoshkrris</a>
+                    height="40"/>Vishal Chauhan</a>
                 &nbsp;
                 <a href="https://api.adviceslip.com/"
                   class="btn btn-outline-dark btn-sm"><i class="fa fa-link" aria-hidden="true"></i> Advice Slip</a>
